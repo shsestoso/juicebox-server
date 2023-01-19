2b61a19742002c9cbbe48be195938246b6395c36
jwalus <walusjacob@gmail.com>
2023-01-19 17:23:41 -0600

    Part 1 all complete

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const apiRouter = express.Router();
 
-const usersRouter = require('./users');
-apiRouter.use('/users', usersRouter);
+const usersRouter = require("./users");
+apiRouter.use("/users", usersRouter);
+
+const postRouter = require("./posts");
+apiRouter.use("/posts", postRouter);
+
+const tagsRouter = require("./tags");
+apiRouter.use("/tags", tagsRouter);
 
 module.exports = apiRouter;
--- api/users.js
+++ api/users.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const usersRouter = express.Router();
 
+const { getAllUsers } = require('../db');
+
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
 
   next(); // THIS IS DIFFERENT
 });
-
-const { getAllUsers } = require('../db');
 
 usersRouter.get('/', async (req, res) => {
     const users = await getAllUsers();
