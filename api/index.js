88015496f0b2ceaa3c66218ef1cde3b84312be73
Shaira grace sestoso <shairagracesestoso@Shairas-Air.attlocal.net>
2023-01-20 13:11:54 -0600

    adding 'working inside of api/index' in part 2'

--- api/index.js
+++ api/index.js
@@ -1,3 +1,35 @@
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../db');
+const { JWT_SECRET } = process.env;
+
+apiRouter.use(async (req, res, next) => {
+    const prefix = 'Bearer ';
+    const auth = req.header('Authorization');
+  
+    if (!auth) { // nothing to see here
+      next();
+    } else if (auth.startsWith(prefix)) {
+      const token = auth.slice(prefix.length);
+  
+      try {
+        const { id } = jwt.verify(token, JWT_SECRET);
+  
+        if (id) {
+          req.user = await getUserById(id);
+          next();
+        }
+      } catch ({ name, message }) {
+        next({ name, message });
+      }
+    } else {
+      next({
+        name: 'AuthorizationHeaderError',
+        message: `Authorization token must start with ${ prefix }`
+      });
+    }
+  });
+  
+
 const express = require('express');
 const apiRouter = express.Router();
 
@@ -9,5 +41,12 @@ apiRouter.use("/posts", postRouter);
 
 const tagsRouter = require("./tags");
 apiRouter.use("/tags", tagsRouter);
+
+apiRouter.use((error, req, res, next) => {
+    res.send({
+      name: error.name,
+      message: error.message
+    });
+  });
 
 module.exports = apiRouter;
